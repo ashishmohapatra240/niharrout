55e45795e92664e578f0ba3a894747fc7ef9fcbd
ashishmohapatra240 <ashishmohapatra240@gmail.com>
2025-01-15 19:48:37 +0530

    Add ProjectMarquee, AboutMe, and Works components

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,15 @@
 import Hero from "@/components/sections/Hero";
+import ProjectMarquee from "@/components/sections/ProjectMarquee";
+import AboutMe from "@/components/sections/AboutMe";
+import Works from "@/components/sections/Works";
 
 export default function Home() {
   return (
     <main>
       <Hero />
+      <ProjectMarquee />
+      <AboutMe />
+      <Works />
     </main>
   );
 }
