c78fcf0a992bea8e0d0474899bc2af5a79eefe44
ashishmohapatra240 <ashishmohapatra240@gmail.com>
2025-04-30 01:16:43 +0530

    Update Navbar padding

--- components/layout/Navbar.tsx
+++ components/layout/Navbar.tsx
@@ -28,7 +28,7 @@ const Navbar = () => {
 
   return (
     <nav className="fixed top-0 left-0 right-0 z-50 bg-white/80 backdrop-blur-md">
-      <div className="container mx-auto px-24 py-4">
+      <div className="container mx-auto px-6 md:px-24 py-4">
         <div className="flex items-center justify-between">
           <Link href="/" className="relative w-24 h-8">
             <Image
