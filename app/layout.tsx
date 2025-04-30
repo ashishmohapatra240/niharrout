bc851fea3d4fea1a7b5dd3e1e1e4039a3fd4a07e
ashishmohapatra240 <ashishmohapatra240@gmail.com>
2025-04-30 11:29:34 +0530

    Add raw.githubusercontent.com in next config and update OG image url

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,7 +22,7 @@ export const metadata: Metadata = {
       "I'm Nihar, a passionate technologist driven by innovation and a commitment to helping businesses thrive. As a skilled web designer, developer, and software architect, I specialize in crafting exceptional digital products that not only look great but also grow seamlessly alongside your ambitions. Throughout my journey, I have collaborated with a diverse range of business domains, guiding startups, medium-sized enterprises, and large-scale organizations in building and scaling their software products. My extensive knowledge and hands-on experience in the tech landscape have enabled me to deliver customized solutions that perfectly align with my client’s specific needs and aspirations.",
     images: [
       {
-        url: "/images/og.png",
+        url: "https://raw.githubusercontent.com/ashishmohapatra240/niharrout/refs/heads/main/public/images/og.png",
         width: 1200,
         height: 630,
       },
